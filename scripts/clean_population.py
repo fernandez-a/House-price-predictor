4d37da3dc9b3614fdc18b470a916bea5a48017e2
fernandez-a <alejandro.fernandezarmas@gmail.com>
2024-04-24 20:10:23 +0200

    add script to clean the population dataset from ayuntamiento de madrid

---
+++ scripts/clean_population.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import glob
+
+csv_files = glob.glob('./data/madrid/raw/population/*.csv')
+
+
+all_neight = pd.DataFrame()
+for file in csv_files:
+    data = pd.read_csv(file)
+    data = data.drop(columns='edad')
+    total = data[data.columns[data.columns.str.contains('total')]].reset_index(drop=True)
+    total.columns = [col.replace('total','') for col in total.columns.values]
+    total.columns = ['_'.join(col.split('_')[1:]) for col in total.columns.values]
+    total.columns = [col.rstrip('_') for col in total.columns]
+    
+    total_values = pd.DataFrame(total.sum())
+    total_values['district'] = total_values.index
+    total_values.reset_index(drop=True,inplace=True)
+    total_values.rename(columns={0:'total'}, inplace=True)
+    total_values = total_values.reindex(columns=['district','total'])
+    total_values['total'] = total_values['total'].astype(int)
+    total_values['district'] = total_values.district.str.title()
+    total_values['district'] = total_values['district'].str.replace('_',' ')
+    total_values.to_csv(f'./data/madrid/cleaned/total_by_{file.split("/")[-1]}', index=False)
+    all_neight = pd.concat([all_neight, total_values], axis=0)
+
+all_neight.to_csv('./data/madrid/cleaned/total_by_neighbourhood.csv', index=False)

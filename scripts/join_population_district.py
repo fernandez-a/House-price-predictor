7c2428e233ce32fe0219c7c8751ba2be4afedacd
fernandez-a <alejandro.fernandezarmas@gmail.com>
2024-06-04 15:30:48 +0200

    redo the joining and cleaning of populations

--- scripts/join_population_district.py
+++ scripts/join_population_district.py
@@ -1,13 +1,12 @@
 import pandas as pd
 import glob
 
-csv_files = glob.glob('./data/madrid/raw/population/*.csv')
+csv_files = glob.glob('../data/madrid/raw/population/population_total_D*.csv')
 
 
 all_neight = pd.DataFrame()
 for file in csv_files:
     data = pd.read_csv(file)
-    data = data.drop(columns='edad')
     total = data[data.columns[data.columns.str.contains('total')]].reset_index(drop=True)
     total.columns = [col.replace('total','') for col in total.columns.values]
     total.columns = ['_'.join(col.split('_')[1:]) for col in total.columns.values]
@@ -21,7 +20,7 @@ for file in csv_files:
     total_values['total'] = total_values['total'].astype(int)
     total_values['district'] = total_values.district.str.title()
     total_values['district'] = total_values['district'].str.replace('_',' ')
-    total_values.to_csv(f'./data/madrid/cleaned/total_by_{file.split("/")[-1]}', index=False)
+    total_values.to_csv(f'../data/madrid/raw/population/total/{file.split("/")[-1]}', index=False)
     all_neight = pd.concat([all_neight, total_values], axis=0)
 
-all_neight.to_csv('./data/madrid/cleaned/total_by_neighbourhood.csv', index=False)
+all_neight.to_csv('../data/madrid/cleaned/total_by_district.csv', index=False)
--- scripts/join_population_neighbourhood.py
+++ scripts/join_population_neighbourhood.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import glob
+import re
 
 csv_files = glob.glob('../data/madrid/raw/population/population_total_D*.csv')
 
@@ -8,18 +9,20 @@ all_neight = pd.DataFrame()
 for file in csv_files:
     data = pd.read_csv(file)
     total = data[data.columns[data.columns.str.contains('total')]].reset_index(drop=True)
-    total.columns = [col.replace('total','') for col in total.columns.values]
-    total.columns = ['_'.join(col.split('_')[1:]) for col in total.columns.values]
+    total.columns = [col.replace('_total','').strip() for col in total.columns.values]
+    pattern = re.compile("\d{2}._")
+    columns_to_drop = [col for col in total.columns if pattern.match(col)]
+    total = total.drop(columns=columns_to_drop)
+    total.columns = ['_'.join(col.split('_')[1:]).capitalize() for col in total.columns]
     total.columns = [col.rstrip('_') for col in total.columns]
     
     total_values = pd.DataFrame(total.sum())
-    total_values['district'] = total_values.index
+    total_values['neigbourhood'] = total_values.index
     total_values.reset_index(drop=True,inplace=True)
     total_values.rename(columns={0:'total'}, inplace=True)
-    total_values = total_values.reindex(columns=['district','total'])
+    total_values = total_values.reindex(columns=['neigbourhood','total'])
     total_values['total'] = total_values['total'].astype(int)
-    total_values['district'] = total_values.district.str.title()
-    total_values['district'] = total_values['district'].str.replace('_',' ')
+    total_values['neigbourhood'] = total_values['neigbourhood'].str.replace('_',' ')
     total_values.to_csv(f'../data/madrid/raw/population/total/{file.split("/")[-1]}', index=False)
     all_neight = pd.concat([all_neight, total_values], axis=0)
 
